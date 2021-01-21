5b260e6c955dd4d8553a31acdd782233b8eb8617
ndjPirisyala <ndjpirisyala96@gmail.com>
2021-01-21 23:39:52 +0530

    mmm

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = '1597162943:AAHcIwH6o_L08I0RC4yauxpfjk-JCqeGuyA'
-Amazon_login = 'ndjpirisyala96@gmail.com'
-Amazon_Password = 'konchi1234'
+TOKEN = '1532451505:AAENM-KzHzNX8P3sK2s1tPIVT2dPD-o8yGo'
+Amazon_login = 'thelust43@gmail.com'
+Amazon_Password = 'respawn49'
 
 main_link = 'https://www.amazon.it/ap/signin?openid.return_to=https%3A%2F%2Fprogramma-affiliazione.amazon.it%2F%3Fopenid.assoc_handle%3Damzn_associates_it%26openid.claimed_id%3Dhttps%253A%252F%252Fwww.amazon.it%252Fap%252Fid%252Famzn1.account.AERGOFTORZTABSJRFOBT4UONSZBA%26openid.identity%3Dhttps%253A%252F%252Fwww.amazon.it%252Fap%252Fid%252Famzn1.account.AERGOFTORZTABSJRFOBT4UONSZBA%26openid.mode%3Did_res%26openid.ns%3Dhttp%253A%252F%252Fspecs.openid.net%252Fauth%252F2.0%26openid.op_endpoint%3Dhttps%253A%252F%252Fwww.amazon.it%252Fap%252Fsignin%26openid.response_nonce%3D2020-12-25T07%253A58%253A38Z4805015803650775821%26openid.return_to%3Dhttps%253A%252F%252Fprogramma-affiliazione.amazon.it%252F%26openid.signed%3Dassoc_handle%252Cclaimed_id%252Cidentity%252Cmode%252Cns%252Cop_endpoint%252Cresponse_nonce%252Creturn_to%252Cns.pape%252Cpape.auth_policies%252Cpape.auth_time%252Csigned%26openid.ns.pape%3Dhttp%253A%252F%252Fspecs.openid.net%252Fextensions%252Fpape%252F1.0%26openid.pape.auth_policies%3Dhttp%253A%252F%252Fschemas.openid.net%252Fpape%252Fpolicies%252F2007%252F06%252Fnone%26openid.pape.auth_time%3D2020-12-25T07%253A58%253A38Z%26openid.sig%3DS7azk78xXO23JzrYzaiG8BGhFbFN1%252FEdRJlIc4eFTUM%253D%26serial%3D%26&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_associates_it&openid.mode=checkid_setup&marketPlaceId=APJ6JRA9NG5V4&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.pape.max_auth_age=0'
 
